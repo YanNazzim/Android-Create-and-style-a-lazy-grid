25c33ad500c9b737dd7bfcad502fd0f470ba69fd
Yan <ygonz42@gmail.com>
2024-04-04 02:22:33 -0400

    Lab Complete

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,5 +22,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun AppScreen() {
+    LazyGrid()
+}
+
+@Composable
+fun LazyGrid() {
+    LazyVerticalGrid(columns = GridCells.Adaptive(160.dp)) {
+        items(1000){
+            MyGridCell()
+        }
+    }
 }
 
